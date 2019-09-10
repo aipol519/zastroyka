55ce4f1f9930752ca73ad18535c3d85f621bd658
Egor Kondichev <bit.kmail@gmail.com>
2019-09-10 22:55:33 +0300

    Initial commit

---
+++ Source/zastroyka/zastroyka.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "zastroyka.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, zastroyka, "zastroyka" );
